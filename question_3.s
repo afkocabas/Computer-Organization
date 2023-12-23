3da348964e14a545ea0739eb718b0b13083871a9
Ahmet Furkan Kocabas <ahmetfurkan.kocabas@agu.edu.tr>
2023-12-23 19:27:53 +0300

    Question 3, function names rearranged properly

--- question_3.s
+++ question_3.s
@@ -7,32 +7,28 @@ _main:
 	li a0, 0					# File descriptor
 	
 	la, a0, prompter			# Load address of the prompter to a0	
-	jal ra, print_string			# print the prompter
+	jal ra, ecall_print_str			# print the prompter
 	
-	jal ra, get_integer			# get number of numbers from the user
+	jal ra, ecall_get_integer			# get number of numbers from the user
 							# a0 has the user input
 	mv a1, a0				# set arguments of get_array_elements
 	jal ra, get_array_elements	# get elements from the user as many as the input
 
-	jal ra, exit				# Exit the program
+	jal ra, ecall_exit				# Exit the program
 	
 	
-get_integer:
-	li a7, 5
-	ecall	
-	jalr zero 0(ra) 
 
 
 get_array_elements: #( input [a1], )
-	
+	mv t1, a1
 	la, a2, array								# a2 == 0(array)
 	get_element:							
-		beqz a1, get_array_exit					# if t1 == 0 exit the function
+		beqz t1, get_array_exit					# if t1 == 0 exit the function
 	
 		addi sp, sp, -4	
 		sw ra, 0(sp)							# store return address to stack
 	
-		jal ra, get_integer 						# a0 = integer entered by user
+		jal ra, ecall_get_integer 						# a0 = integer entered by user
 	
 		lw ra, 0(sp)							# load back return address of the caller from stack
 		addi sp, sp, 4
@@ -45,29 +41,13 @@ get_array_elements: #( input [a1], )
 		lw ra, 0(sp)							# load back return address of the caller from stack
 		addi sp, sp, 4
 	
-		addi a1, a1, -1							# a1--
+		addi t1, t1, -1							# a1--
 		addi a2, a2, 4
 		beq x0, x0, get_element
 	
 	get_array_exit:
 		jalr zero, 0(ra)
-	
 
-	
-exit:
-	li a7, 93
-	ecall
-	jalr x0 0(x1)
-	
-print_string: 
-	li a7, 4
-	ecall
-	jalr x0 0(x1)
-
-print_integer:
-	li a7, 1
-	ecall
-	jalr zero, 0(ra)
 	
 
 
@@ -75,12 +55,49 @@ add_element_to_the_array: # (a0 = value,  a2 = address)
 		sw a0, 0(a2)
 		jalr zero, 0(ra)
 
-print_array:
-	la a2, array			# a2 has the adress of the array
-	print_element:
-		
-	
+get_element_from_the_array:  # (a0 = value,  a2 = address)
+		lw a0, 0(a2)
+		jalr zero, 0(ra)
 
+print_array: # (a1 = the number of elements to print)
+
+	la a2, array							# a2 has the adress of the array[0]
+	mv t1, a1								#t1 is the number of elements to print				
+	print_element:
+		beqz t1, print_array_exit			 # if t1 == 0 stop printing
+		
+		addi sp, sp, -4	
+		sw ra, 0(sp)						# store return address to stack
+		
+		jal ra,  get_element_from_the_array	# the element is in a0
+		
+		jal ra, ecall_print_int				# print the element in a0
+		
+
+		
+	print_array_exit:
+		jalr zero, 0(ra)
+
+# Helper Functions			
+ecall_exit:
+	li a7, 93
+	ecall
+	jalr x0 0(x1)
+	
+ecall_print_str: 
+	li a7, 4
+	ecall
+	jalr x0 0(x1)
+
+ecall_print_int: #(a0 = integer)
+	li a7, 1
+	ecall
+	jalr zero, 0(ra)
+
+ecall_get_integer:
+	li a7, 5
+	ecall	
+	jalr zero 0(ra) 
 				
 .data
 	next_line: .asciz " \n"
