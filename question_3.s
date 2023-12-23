b532e36090bcf85ae9726607664e126706293a98
Ahmet Furkan Kocabas <ahmetfurkan.kocabas@agu.edu.tr>
2023-12-23 21:13:15 +0300

    Question 3 - Solved!

--- question_3.s
+++ question_3.s
@@ -14,6 +14,8 @@ _main:
 	mv a1, a0					# set arguments of get_array_elements
 	jal ra, get_array_elements		# get elements from the user as many as the input
 	
+	jal ra, bubble_sort
+	
 	jal ra, print_array				# prints the array
 
 	jal ra, ecall_exit				# Exit the program
@@ -22,34 +24,67 @@ _main:
 
 get_array_elements: #( input [a1], )
 	mv t1, a1
-	la, a2, array								# a2 == 0(array)
+	la, a2, array										# a2 == 0(array)
 	get_element:							
 		beqz t1, get_array_elements_exit					# if t1 == 0 exit the function
 	
 		addi sp, sp, -4	
-		sw ra, 0(sp)							# store return address to stack
+		sw ra, 0(sp)									# store return address to stack
 	
 		jal ra, ecall_get_integer 						# a0 = integer entered by user
 	
-		lw ra, 0(sp)							# load back return address of the caller from stack
+		lw ra, 0(sp)									# load back return address of the caller from stack
 		addi sp, sp, 4
 	
 		addi sp, sp, -4	
-		sw ra, 0(sp)							# store return address to stack
+		sw ra, 0(sp)									# store return address to stack
 	
-		jal ra, array_append 					# add a0 to array
+		jal ra, array_append 							# add a0 to array
 	
-		lw ra, 0(sp)							# load back return address of the caller from stack
+		lw ra, 0(sp)									# load back return address of the caller from stack
 		addi sp, sp, 4
 	
-		addi t1, t1, -1							# a1--
-		addi a2, a2, 4							# set the address to the next word
+		addi t1, t1, -1									# a1--
+		addi a2, a2, 4									# set the address to the next word
 		j get_element
 	
 	get_array_elements_exit:
 		jalr zero, 0(ra)
 
-
+bubble_sort: # bubbleSort( a1 = number of elements in the array, )
+	la a2, array							# saves the address of the array to a2
+	addi a3, a1, -1							# a3 = i = length - 1
+	
+	outer_loop:							# for(i = 0; i < length - 1; i++) ## double check here possible logic mistake
+	
+		beqz a3, end_outer_loop			# if( a3 == 0) exit outer loop
+		
+		addi a4, zero, 0 
+		
+		inner_loop:						# for(j = i; j < length - 1; j++)
+			
+			bge a4, a3 end_inner_loop		# if(j >= i) exit inner loop
+			slli a5, a4, 2  					# a4 = a4 * 4
+			add a5, a2, a5  				# a5 = &array[j]
+			lw a6 0(a5)					# a6 = array[j]
+			lw a7, 4(a5)					# a7 = array[j +1]
+			blt a7, a6, increment_j			# if (array[j + 1] < array[j]) => no need to swap
+		
+			swap:						# if( array[j] > array[j+1])
+				sw a6, 4(a5)  				# array[i + 1] = array[i]
+    				sw a7, 0(a5)  				# array[i] = array[i + 1]			
+			increment_j:
+				addi a4, a4, 1  			# j++	
+				j inner_loop			
+		end_inner_loop:
+		
+		addi a3, a3, -1						#i--
+		j outer_loop
+	end_outer_loop:
+		
+	exit_bubble_sort:
+		jalr zero 0(ra)
+	
 
 print_array: # printArray(a1 = the number of elements to print)
 
